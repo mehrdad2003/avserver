3db09749c08b45e67f98c3ef28bae28a5c3015b4
unknown <mehrdadzare2003@gmail.com>
2023-05-22 11:47:32 +0430

    sder12

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ const port = process.env.SERVER_PORT;
 
   app.use(cors());
 
-
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 app.get("/api/v1/runway/:icao", runwayAPI);
 
 app.listen(port, () => {
